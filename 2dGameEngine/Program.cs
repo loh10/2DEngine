0953a9d9e782186248b6b1cbad7cc98323b85747
Loh10 <naturellement.toi@gmail.com>
2025-10-13 16:21:45 +0200

    add all file

---
+++ 2dGameEngine/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameEngine
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using Game game = new Game();
+            game.Run();
+        }
+    }
+}
